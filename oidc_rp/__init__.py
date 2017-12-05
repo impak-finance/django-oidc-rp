43232b5515490ead6546a36942cf12179b0b2db7
Morgan Aubert <morgan.aubert@impakfinance.com>
2017-12-05 18:11:29 -0500

    Updated init module

--- oidc_rp/__init__.py
+++ oidc_rp/__init__.py
@@ -1,4 +1,13 @@
 __version__ = '0.1.0.dev'
 
+# Deploying a new version:
+# 1. remove the ".dev" from the current version number
+# 2. create a new commit named (eg. "Prepared 0.1.1 version")
+# 3. run "git tag x.y.z" (eg. "git tag 0.1.1")
+# 4. run "python setup.py sdist bdist_wheel upload"
+# 5. bump the version (increment the version and append a ".dev" to it). eg. "0.1.2.dev"
+# 6. create a new commit (eg. "Bumped version to 0.1.2.dev")
+# 7. run "git push" and "git push --tags"
+
 
 default_app_config = 'oidc_rp.apps.OIDCRelyingPartyAppConfig'
