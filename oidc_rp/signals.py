3d7cd338be3dc646f69db6ade3688053a42d6f15
Morgan Aubert <morgan.aubert@zoho.com>
2018-04-15 10:58:06 -0400

    Added missing docstring

--- oidc_rp/signals.py
+++ oidc_rp/signals.py
@@ -1,3 +1,12 @@
+"""
+    OpenID Connect relying party (RP) signals
+    =========================================
+
+    This modules defines Django signals that can be triggered during the OpenID Connect
+    authentication process.
+
+"""
+
 from django.dispatch import Signal
 
 
