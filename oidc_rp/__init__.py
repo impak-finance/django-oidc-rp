f950eaecf7a9c24df8a5d3c8d6e8bc06e9e4ab38
Maciej Zakrzewski <maciej.zakrzewski@polcode.net>
2022-06-09 11:44:48 +0200

    updated deploy text in __init__

--- oidc_rp/__init__.py
+++ oidc_rp/__init__.py
@@ -4,7 +4,7 @@ __version__ = '0.4.0.dev0'
 # 1. remove the ".dev" from the current version number
 # 2. create a new commit (eg. "Prepared 0.1.1 release")
 # 3. run "git tag x.y.z" (eg. "git tag 0.1.1")
-# 4. run "python setup.py sdist bdist_wheel upload"
+# 4. run `make build_and_deploy`
 # 5. bump the version (increment the version and append a ".dev" to it). eg. "0.1.2.dev"
 # 6. create a new commit (eg. "Bumped version to 0.1.2.dev")
 # 7. run "git push" and "git push --tags"
